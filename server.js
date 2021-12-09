274704471ec307942276a01cd2e31b41ff5b02a5
songmoku <40141633+songmoku@users.noreply.github.com>
2021-12-09 15:46:49 +0900

    confused

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
+/* eslint-disable no-unused-vars */
+// @ts-check
 const http = require('http')
 
 const PORT = 4000
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello!',
+  },
+  {
+    id: 'Second_post',
+    title: 'My seconde post',
+    content: 'second',
+  },
+]
 
 /**
  * post
@@ -10,10 +33,14 @@ const PORT = 4000
  * POST /posts
  */
 const server = http.createServer((req, res) => {
+  const REGEX_ID = /^\/posts\/([a-zA-Z0-9-_]+)$/ // () Capture Group
+  const postIdRexResult = (req.url && REGEX_ID.exec(req.url)) || undefined
+
   if (req.url === '/posts' && req.method === 'GET') {
     res.statusCode = 200
     res.end('List of Posts')
-  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+  } else if (postIdRexResult) {
+    console.log(REGEX_ID.exec(req.url))
     res.statusCode = 200
     res.end('A content of the posts')
   } else if (req.url === '/posts' && req.method === 'POST') {
