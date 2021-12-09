349d0273bffb1beeb2fdca22a18577d7eee07bb7
songmoku <songmoku@gmail.com>
2021-12-09 22:26:44 +0900

    remote first

---
+++ new.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'first node',
+    title: 'first title',
+    content: 'content 1',
+  },
+]
